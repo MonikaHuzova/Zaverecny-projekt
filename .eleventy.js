b901a4c53dfbb12d349fad14f85cf155c9b75012
MonikaHuzova <huzova.monika@gmail.com>
2024-12-02 14:00:26 +0100

    stylovani

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     // Výchozí výstupní složka je: _site
   
     // Zkopírovat images/ do _site/images
-    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("photo");
     
     // Zkopírovat css/ to _site/css/
     eleventyConfig.addPassthroughCopy("css");
